af6a1fe4d87c2fa6f039bc0c0501fe3a49b74c76
hela karaa <hela.karaa@ensi-uma.tn>
2018-07-30 13:49:53 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,9 +4,12 @@ def turn_count (board)
     if index=="X" || index=="O"
      counter=+1
     end
-  
+  return counter
 end
 
 def current_player(board)
-
+   if turn_count(board)%2==0
+     return "X"
+   else 
+     return "O"
 end
